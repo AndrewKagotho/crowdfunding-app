e4e1fd2f2559282294f54a97a43e016fc0b62308
Andrew G. Kagotho <86290395+AndrewKagotho@users.noreply.github.com>
2022-11-26 19:40:47 +0300

    Further dev

--- src/utils/php/dbconn.php
+++ src/utils/php/dbconn.php
@@ -1,9 +1,9 @@
 <?php
-  header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Headers: Accept");
-  header("Access-Control-Allow-Methods: *");
-  header("Content-Type: application.json; charset=UTF-8");
-  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  // header("Access-Control-Allow-Origin: *");
+  // header("Access-Control-Allow-Headers: Accept");
+  // header("Access-Control-Allow-Methods: *");
+  // header("Content-Type: application.json; charset=UTF-8");
+  // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
   $dbserver = 'containers-us-west-84.railway.app:5450';
   $dbuser = 'root';
