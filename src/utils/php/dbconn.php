13eb74e444d605dd5d89a491ade4245a478c6756
Andrew G. Kagotho <86290395+AndrewKagotho@users.noreply.github.com>
2022-11-26 19:36:15 +0300

    Further dev

--- src/utils/php/dbconn.php
+++ src/utils/php/dbconn.php
@@ -1,7 +1,7 @@
 <?php
   header("Access-Control-Allow-Origin: *");
-  header("Access-Control-Allow-Headers: access");
-  header("Access-Control-Allow-Methods: POST, GET");
+  header("Access-Control-Allow-Headers: Accept");
+  header("Access-Control-Allow-Methods: *");
   header("Content-Type: application.json; charset=UTF-8");
   header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
