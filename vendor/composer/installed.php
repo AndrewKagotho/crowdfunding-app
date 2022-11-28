95991be43436a62aad0c34c38b911bef745e9aae
Andrew G. Kagotho <86290395+AndrewKagotho@users.noreply.github.com>
2022-11-28 10:16:48 +0300

    Further dev

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '002c9b3b1f6622e250e7d60a9cfa735cd7a5a82b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '002c9b3b1f6622e250e7d60a9cfa735cd7a5a82b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
