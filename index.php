f5ff0c86c171014d2ca643ae3c3c49662235161d
Andrew G. Kagotho <86290395+AndrewKagotho@users.noreply.github.com>
2022-11-28 10:50:09 +0300

    Further dev

--- index.php
+++ index.php
@@ -14,4 +14,20 @@
     echo 'Connection successful';
   if(!$conn)
     echo 'Connection unsuccessful';
+
+  $resArray = [];
+  $counter = 0;
+
+  // require './index.php';
+
+  $sql = 'SELECT * FROM `heroku_cd39df722fedf47.pledges`';
+
+  $result = mysqli_query($conn, $sql);
+
+  while($object = mysqli_fetch_object($result)) {
+    $resArray[$counter] = $object;
+    $counter++;
+  }
+
+  echo json_encode($resArray);
 ?>
