8f2b6ce116e34140fb27444a3193b281742e4f48
Andrew G. Kagotho <86290395+AndrewKagotho@users.noreply.github.com>
2022-12-06 20:23:33 +0300

    Further dev

--- src/api/index.php
+++ src/api/index.php
@@ -9,12 +9,12 @@
   //   header("Content-Type: application.json; charset=UTF-8");
   // }
 
-  $dbhost = 'eu-cdbr-west-03.cleardb.net';
-  $dbuser = 'b1c5af0d406b2d';
-  $dbpass = '678ea297';
-  $dbname = 'heroku_cd39df722fedf47';
+  // $dbhost = 'eu-cdbr-west-03.cleardb.net';
+  // $dbuser = 'b1c5af0d406b2d';
+  // $dbpass = '678ea297';
+  // $dbname = 'heroku_cd39df722fedf47';
 
-  var_dump($_SERVER);
+  // var_dump($_SERVER);
 
   // if ($_SERVER['HTTP_HOST'] == 'localhost') {
   //   $dbhost = 'localhost';
@@ -23,10 +23,10 @@
   //   $dbname = 'crowdfundingdb';
   // }
 
-  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+  // $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-  if($conn)
-    echo 'Connection successful';
-  if(!$conn)
-    echo 'Connection unsuccessful';
+  // if($conn)
+  //   echo 'Connection successful';
+  // if(!$conn)
+  //   echo 'Connection unsuccessful';
 ?>
